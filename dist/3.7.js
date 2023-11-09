bf61d41800dc00e07b92ab2d2df2b97ef25d9e0c
mahdimortuza <mahdi.webx@gmail.com>
2023-11-09 14:14:46 +0600

    static

---
+++ dist/3.7.js
@@ -0,0 +1,19 @@
+"use strict";
+{
+    // static 
+    class Counter {
+        static increment() {
+            return Counter.count = Counter.count + 1;
+        }
+        static decrement() {
+            return Counter.count = Counter.count - 1;
+        }
+    }
+    Counter.count = 0;
+    const instance1 = new Counter();
+    console.log(Counter.increment());
+    const instance2 = new Counter();
+    console.log(Counter.increment());
+    const instance3 = new Counter();
+    console.log(Counter.increment());
+}
